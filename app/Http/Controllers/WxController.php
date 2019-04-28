3f35581d3717715e472700bd9c5ce9c5116a1f67
LC0089 <1652324132@qq.com>
2019-04-28 08:25:42 +0000

    fdsbfgf

--- app/Http/Controllers/WxController.php
+++ app/Http/Controllers/WxController.php
@@ -65,6 +65,37 @@ class WxController extends Controller{
             </xml>";
                 echo $str;
             }
+
+            $good = DB::table('shop_goods')->where('goods_up',1)->orderBy('create_time','desc')->first();
+
+            $jsapi_ticket = $this->JsapiTicket();
+            $nonceStr = Str::random(10);
+            $timestamp = time();
+            $current_cul = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+            $string1 = "jsapi_ticket=$jsapi_ticket&noncestr=$nonceStr&timestamp=$timestamp&url=$current_cul";
+            $sign = sha1($string1);
+//        dump($sign);die;
+            $js_config = [
+                'appId'=>env('WX_APPID'), //公众号IP
+                'timestamp'=>$timestamp,
+                'nonceStr'=>$nonceStr,  //随机字符串
+                'signature'=>$sign,  //签名
+//            'jsApiList'=>['chooseImage'],  //要使用的表功能
+            ];
+//        print_r($js_config);die;
+            $picurl = "http://1809lancong.comcto.com/goodsimg/$good->goods_img";
+//        print_r($picurl);die;
+            $url = "http://1809lancong.comcto.com/goodDetail";
+            $title = "秀儿";
+            $desc = "啊哈哈哈";
+            $arr = [
+                'picurl'=>$picurl,
+                'url'=>$url,
+                'title'=>$title,
+                'desc'=>$desc
+            ];
+            return view('goods.detail',['good'=>$good,'js_config'=>$js_config,'arr'=>$arr]);
+
         }
 
         if($Event=='subscribe'){
@@ -97,35 +128,6 @@ class WxController extends Controller{
                 echo $str;
             }
 
-            $good = DB::table('shop_goods')->where('goods_up',1)->orderBy('create_time','desc')->first();
-
-            $jsapi_ticket = $this->JsapiTicket();
-            $nonceStr = Str::random(10);
-            $timestamp = time();
-            $current_cul = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-            $string1 = "jsapi_ticket=$jsapi_ticket&noncestr=$nonceStr&timestamp=$timestamp&url=$current_cul";
-            $sign = sha1($string1);
-//        dump($sign);die;
-            $js_config = [
-                'appId'=>env('WX_APPID'), //公众号IP
-                'timestamp'=>$timestamp,
-                'nonceStr'=>$nonceStr,  //随机字符串
-                'signature'=>$sign,  //签名
-//            'jsApiList'=>['chooseImage'],  //要使用的表功能
-            ];
-//        print_r($js_config);die;
-            $picurl = "http://1809lancong.comcto.com/goodsimg/$good->goods_img";
-//        print_r($picurl);die;
-            $url = "http://1809lancong.comcto.com/goodDetail";
-            $title = "秀儿";
-            $desc = "啊哈哈哈";
-            $arr = [
-                'picurl'=>$picurl,
-                'url'=>$url,
-                'title'=>$title,
-                'desc'=>$desc
-            ];
-            return view('goods.detail',['good'=>$good,'js_config'=>$js_config,'arr'=>$arr]);
         }
 
         if($MsgType=='image'){
